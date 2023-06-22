b68174efc204a919c84c40600aa8edc425d8c79d
salmart <salatiel@umich.edu>
2023-06-22 15:29:13 -0400

    xxx

---
+++ build/ldlidar/CMakeFiles/ldlidar.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ldlidar.dir/src/cmd_interface_linux.cpp.o"
+  "CMakeFiles/ldlidar.dir/src/ldlidar.cpp.o"
+  "CMakeFiles/ldlidar.dir/src/ldlidar_node.cpp.o"
+  "CMakeFiles/ldlidar.dir/src/lipkg.cpp.o"
+  "CMakeFiles/ldlidar.dir/src/tofbf.cpp.o"
+  "ldlidar"
+  "ldlidar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ldlidar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
